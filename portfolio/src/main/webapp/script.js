f173caf49447f1d026720c12651b28ed6c35ac26
Tzirliya <tzirliya@yahoo.com>
2020-06-26 14:10:41 +0000

    Move javascript from the end of html files into the javascript file.

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -33,4 +33,18 @@ function addRandomFact() {
   factContainer.innerText = fact;
 }
 
-
+function makeCollapsible() {
+    var coll = document.getElementsByClassName("collapsible");
+      var i;
+      for (i = 0; i < coll.length; i++) {
+        coll[i].addEventListener("click", function() {
+          this.classList.toggle("active");
+          var content = this.nextElementSibling;
+          if (content.style.display === "block") {
+            content.style.display = "none";
+          } else {
+            content.style.display = "block";
+          }
+        });
+      }
+}
