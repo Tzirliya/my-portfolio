8988d1450376fbecc9c0cf7b5f84cdedd27737de
Tzirliya <tzirliya@yahoo.com>
2020-07-13 15:14:01 +0000

    Clean up code.

--- portfolio/src/main/java/com/google/sps/servlets/DeleteDataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DeleteDataServlet.java
@@ -11,9 +11,9 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+ 
 package com.google.sps.servlets;
-
+ 
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
@@ -35,49 +35,48 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.text.*;
-
+ 
 /** Servlet that returns some example content. TODO: modify this file to handle comments data */
 @WebServlet("/delete-data")
 public class DeleteDataServlet extends HttpServlet {
-
+ 
   @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    UserService userService = UserServiceFactory.getUserService();
+    String isAdmin = String.valueOf(userService.isUserAdmin());
     String deleteAll = (String) request.getParameter("deleteAll");
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    List<Key> keys = new ArrayList<>();
     if (deleteAll.equals("true")) {
-      UserService userService = UserServiceFactory.getUserService();
-      String isAdmin = String.valueOf(userService.isUserAdmin());
       if (isAdmin.equals("true")){
         Query query = new Query("Comment").setKeysOnly();
-        DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
         PreparedQuery results = datastore.prepare(query);  
-        List<Key> keys = new ArrayList<>();  
         for (Entity entity : results.asIterable()) {
-        keys.add(entity.getKey());
+          keys.add(entity.getKey());
         }
-        datastore.delete(keys);
-        System.out.println("Deleted all comments");
+        System.out.println("Delete all comments");
       }
     } else {
       Query query = new Query("Comment");
-      DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
       PreparedQuery results = datastore.prepare(query);
-      UserService userService = UserServiceFactory.getUserService();
       String userId = userService.getCurrentUser().getUserId();
-      String isAdmin = String.valueOf(userService.isUserAdmin());
       long id = Long.parseLong(request.getParameter("id"));
       for (Entity entity : results.asIterable()) {
         String e_userId = (String) entity.getProperty("userId");
         long e_id = (long) entity.getKey().getId();
         if (e_id == id && (e_userId.equals(userId) || isAdmin.equals("true"))){
           Key key = entity.getKey();
-          datastore.delete(key);
-          System.out.println("Deleted a comment");
+          keys.add(key);
+          System.out.println("Delete a comment");
           break;
         }
       }
     }
+    datastore.delete(keys);
     // Redirect back to the HTML page.
     response.sendRedirect("/feedback.html");
   }
   
 }
+ 
+
